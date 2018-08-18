75e13314e1fc8cf11f950c0cf8154c40e015062d
Nathan Campos <eeepc904@gmail.com>
2018-08-18 15:38:56 -0300

    Improved the layout of the project container a bit

--- index.php
+++ index.php
@@ -64,9 +64,51 @@ require_once $inter->get_lang_include($_SERVER["PHP_SELF"]);
 
 		<!-- Projects -->
 		<div class="project-container">
-			<h1>Hello!</h1>
+			<hr>
+			<h1>PortaStation</h1>
+			<p>Uma estação de solda portátil, única no mercado, capaz de ser utilizada como uma estação primária em uma bancada de eletrônica, em um ambiente industrial de produção ou ser levada a campo para realização de instalações, reparos e manutenção.</p>
+
+			<div class="project-image-carousel container">
+				<div class="row">
+					<div class="col">
+						<img src="images/projects/power12/show.jpg" class="img-fluid img-thumbnail">
+					</div>
+					<div class="col">
+						<img src="images/projects/power12/open.jpg" class="img-fluid img-thumbnail">
+					</div>
+					<div class="col">
+						<img src="images/projects/power12/open-connected.jpg" class="img-fluid img-thumbnail">
+					</div>
+				</div>
+
+				<br>
+			</div>
+
+			<p>Um amplificador stereo compacto de 6W por canal completamente discreto, ou seja, todo o circuito foi feito sem utilizar nenhum tipo de circuito integrado. Este foi o meu segundo amplificador e foi desenvolvido com o intuito de aprender mais sobre eletrônica analógica e amplificadores operacionais discretos. Este projeto é utilizado diariamente desde 2014 como a principal saída de áudio do meu computador e em todos esses anos nunca teve nenhum tipo de falha e continua tendo um som extremamente livre de distorções com um THD medido de 0.03% em 2016.</p>
+
+			<b>PORTÁTIL E À BATERIA</b>
+			<p>Além de ser pequena e extremamente leve pesando apenas 340g, a estação difere das outras no mercado por ser capaz de operar remotamente utilizando uma bateria, perfeita para ser utilizada em campo.</p>
+
+			<b>INTERFACE MODERNA E INTUITIVA</b>
+			<p>Muitas estações de solda no mercado possuem apenas um knob para ajuste da temperatura de forma analógica, onde a seleção é impressa ao redor knob, ou nas versões digitais possuem um visor de LED de 7 segmentos e botões para aumentar ou diminuir a temperatura. Porém a nossa estação utiliza um display gráfico capaz de apresentar a temperatura de solda, além de muitas outras informações importantes, também possui um menu de configuração para personalização.</p>
+
+			<div class="schematic-container">
+				<h4>Schematic and Board</h4>
+
+				<div class="project-image-carousel container">
+					<div class="row">
+						<div class="col">
+							<img src="images/projects/power12/schematic.png" class="img-fluid img-thumbnail">
+						</div>
+						<div class="col">
+							<img src="images/projects/power12/board.png" class="img-fluid img-thumbnail">
+						</div>
+					</div>
+
+					<br>
+				</div>
+			</div>
 		</div>
-	</div>
 	</div>
 
 	<!-- Footer -->
