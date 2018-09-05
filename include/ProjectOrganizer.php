c35d3e759ffe9b419f78fdf712f8eea18585ca0d
Nathan Campos <eeepc904@gmail.com>
2018-09-04 22:41:21 -0300

    Parsing all the stuff needed for creating a project object

--- include/ProjectOrganizer.php
+++ include/ProjectOrganizer.php
@@ -8,10 +8,88 @@
 
 require_once "config.php";
 
+class Project {
+	const IMAGE_FILTER = '/\.(png|jpg|jpeg|bmp|gif)$/';
+	const IMAGE_CATEGORIES = array("main", "board", "schematic", "misc");
+
+	public $id;
+	public $root;
+
+	public $name;
+	public $brief;
+	public $description;
+	public $images;
+
+	/**
+	 * Creates a project object.
+	 *
+	 * @param string $id   Project ID.
+	 * @param string $root Project root directory.
+	 */
+	function __construct($id, $root = NULL) {
+		$this->id = $id;
+
+		// Populate the class if the project exists.
+		if (!is_null($root)) {
+			$this->root = "$root/$id";
+
+			$this->parse_definition();
+			$this->description = $this->get_main_description();
+
+			foreach (self::IMAGE_CATEGORIES as $cat) {
+				$this->images[$cat] = $this->parse_images($cat);
+			}
+		}
+	}
+
+	/**
+	 * Parses the definition file and populates the class with the contents.
+	 */
+	private function parse_definition() {
+		$project = json_decode(file_get_contents($this->root . "/project.json"), true);
+
+		$this->name = $project["name"];
+		$this->brief = $project["brief"];
+	}
+
+	/**
+	 * Parses the images for a given category.
+	 *
+	 * @param  string $category Image category.
+	 * @return array            List of relative image paths.
+	 */
+	private function parse_images($category) {
+		$image_list = array();
+		$relpath = "images/$category";
+
+		foreach (glob($this->root . "/$relpath/*") as $image) {
+			if (preg_match(self::IMAGE_FILTER, $image)) {
+				// We got a valid image, now lets make it into a relative path.
+				preg_match("/(projects\/" . $this->id ."\/images.+)/", $image, $matches);
+				array_push($image_list, $matches[1]);
+			}
+		}
+
+		return $image_list;
+	}
+
+	/**
+	 * Gets the main project description.
+	 *
+	 * @return string HTML description of the project.
+	 */
+	private function get_main_description() {
+		return file_get_contents($this->root . "/description.html");
+	}
+}
+
 class ProjectOrganizer {
 	private $project_dir;
 	private $project_list;
 
+	/**
+	 * Constructs the project organizer class.
+	 */
 	function __construct() {
 		// Set projects root directory.
 		$this->project_dir = $_SERVER["DOCUMENT_ROOT"] . Config::WEBSITE_ROOT . "/projects";
@@ -19,10 +97,16 @@ class ProjectOrganizer {
 		// Grab the project directories.
 		$this->project_list = array();
 		foreach (glob($this->project_dir . '/*', GLOB_ONLYDIR) as $project) {
-			array_push($this->project_list, basename($project));
+			if (!preg_match('/\.ignore$/', $project)) {
+				array_push($this->project_list, basename($project));
+			}
 		}
 
 		print_r($this->project_list);
+		foreach ($this->project_list as $id) {
+			$project = new Project($id, $this->project_dir);
+			print_r($project);
+		}
 	}
 }
 
